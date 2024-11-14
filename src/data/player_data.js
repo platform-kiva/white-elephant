e1268f75bfe92d886018ce6105da74afae8796c8
Kiva Loor <kivaloor@MBP.lan>
2024-11-13 21:38:58 -0500

    update names and presents test

--- src/data/player_data.js
+++ src/data/player_data.js
@@ -22,87 +22,32 @@ Victoria
 
 export const PLAYER_DATA = [
     {
-        name: 'Akari',
-        id: null,
-        presentHistory: []
-    },
-    {
-        name: 'Carlos',
-        id: null,
-        presentHistory: []
-    },
-    {
-        name: 'Duk-Kyu',
-        id: null,
-        presentHistory: []
-    },
-    {
-        name: 'Dylan',
-        id: null,
-        presentHistory: []
-    },
-    {
-        name: 'Evan',
-        id: null,
-        presentHistory: []
-    },
-    {
-        name: 'Grace',
-        id: null,
-        presentHistory: []
-    },
-    {
-        name: 'Jake',
-        id: null,
-        presentHistory: []
-    },
-    {
-        name: 'John',
-        id: null,
-        presentHistory: []
-    },
-    {
-        name: 'Ken',
-        id: null,
-        presentHistory: []
-    },
-    {
         name: 'Kiva',
         id: null,
         presentHistory: []
     },
     {
-        name: 'Mica',
+        name: 'Moon',
         id: null,
         presentHistory: []
     },
     {
-        name: 'Michelle',
+        name: 'Seb',
         id: null,
         presentHistory: []
     },
     {
-        name: 'Mya',
+        name: 'Mesh',
         id: null,
         presentHistory: []
     },
     {
-        name: 'Nicole',
+        name: 'Spence',
         id: null,
         presentHistory: []
     },
     {
-        name: 'Sergio',
-        id: null,
-        presentHistory: []
-    },
-    {
-        name: 'Tammy',
-        id: null,
-        presentHistory: []
-    },
-    {
-        name: 'Victoria',
+        name: 'Dave',
         id: null,
         presentHistory: []
     }
--- src/data/present_data.js
+++ src/data/present_data.js
@@ -4,17 +4,17 @@ import P2_C from '../assets/present-covers/Gift03.png'
 import P3_C from '../assets/present-covers/Gift04.png'
 import P4_C from '../assets/present-covers/Gift05.png'
 import P5_C from '../assets/present-covers/Gift06.png'
-import P6_C from '../assets/present-covers/Gift07.png'
-import P7_C from '../assets/present-covers/Gift08.png'
-import P8_C from '../assets/present-covers/Gift09.png'
-import P9_C from '../assets/present-covers/Gift10.png'
-import P10_C from '../assets/present-covers/Gift11.png'
-import P11_C from '../assets/present-covers/Gift12.png'
-import P12_C from '../assets/present-covers/Gift13.png'
-import P13_C from '../assets/present-covers/Gift14.png'
-import P14_C from '../assets/present-covers/Gift15.png'
-import P15_C from '../assets/present-covers/Gift16.png'
-import P16_C from '../assets/present-covers/Gift17.png'
+// import P6_C from '../assets/present-covers/Gift07.png'
+// import P7_C from '../assets/present-covers/Gift08.png'
+// import P8_C from '../assets/present-covers/Gift09.png'
+// import P9_C from '../assets/present-covers/Gift10.png'
+// import P10_C from '../assets/present-covers/Gift11.png'
+// import P11_C from '../assets/present-covers/Gift12.png'
+// import P12_C from '../assets/present-covers/Gift13.png'
+// import P13_C from '../assets/present-covers/Gift14.png'
+// import P14_C from '../assets/present-covers/Gift15.png'
+// import P15_C from '../assets/present-covers/Gift16.png'
+// import P16_C from '../assets/present-covers/Gift17.png'
 
 import P0_P from '../assets/present-imgs/1_GlazedDonutBlanket.png'
 import P1_P from '../assets/present-imgs/2_VegetableChopper.png'
@@ -22,17 +22,17 @@ import P2_P from '../assets/present-imgs/3_DeskCoffeeWarmer.png'
 import P3_P from '../assets/present-imgs/4_CatColoringBook.png'
 import P4_P from '../assets/present-imgs/5_BuildOnBrickCoffeeMug.png'
 import P5_P from '../assets/present-imgs/6_MagicTapAutomaticDrinkDispenser.png'
-import P6_P from '../assets/present-imgs/7_BelgianWaffleMaker.png'
-import P7_P from '../assets/present-imgs/8_EchoPopSpeaker.png'
-import P8_P from '../assets/present-imgs/9_BeerMittenGloves.png'
-import P9_P from '../assets/present-imgs/10_WestworldS1_4KUHDBluRay.png'
-import P10_P from '../assets/present-imgs/11_LightsaberChopsticks.png'
-import P11_P from '../assets/present-imgs/12_BreadShapedPillow.png'
-import P12_P from '../assets/present-imgs/13_BambooCheeseBoard.png'
-import P13_P from '../assets/present-imgs/14_ReusablePaperTowels.png'
-import P14_P from '../assets/present-imgs/15_AcupressureMat.png'
-import P15_P from '../assets/present-imgs/16_NachosaurusChipAndDipSet.png'
-import P16_P from '../assets/present-imgs/17_SnuggieWearableBlanket.png'
+// import P6_P from '../assets/present-imgs/7_BelgianWaffleMaker.png'
+// import P7_P from '../assets/present-imgs/8_EchoPopSpeaker.png'
+// import P8_P from '../assets/present-imgs/9_BeerMittenGloves.png'
+// import P9_P from '../assets/present-imgs/10_WestworldS1_4KUHDBluRay.png'
+// import P10_P from '../assets/present-imgs/11_LightsaberChopsticks.png'
+// import P11_P from '../assets/present-imgs/12_BreadShapedPillow.png'
+// import P12_P from '../assets/present-imgs/13_BambooCheeseBoard.png'
+// import P13_P from '../assets/present-imgs/14_ReusablePaperTowels.png'
+// import P14_P from '../assets/present-imgs/15_AcupressureMat.png'
+// import P15_P from '../assets/present-imgs/16_NachosaurusChipAndDipSet.png'
+// import P16_P from '../assets/present-imgs/17_SnuggieWearableBlanket.png'
 
 export const PRESENT_DATA = [
     {
@@ -89,103 +89,103 @@ export const PRESENT_DATA = [
         stealsLeft: 3,
         bgCol: '131, 96, 167'
     },
-    {
-        name: "Belgian Waffle Maker",
-        id: 6,
-        ownerHistory: [],
-        coverImg: P6_C,
-        presentImg: P6_P,
-        stealsLeft: 3,
-        bgCol: '239, 114, 41'
-    },
-    {
-        name: "Echo Pop Speaker",
-        id: 7,
-        ownerHistory: [],
-        coverImg: P7_C,
-        presentImg: P7_P,
-        stealsLeft: 3,
-        bgCol: '0, 153, 190'
-    },
-    {
-        name: "Beer Gloves",
-        id: 8,
-        ownerHistory: [],
-        coverImg: P8_C,
-        presentImg: P8_P,
-        stealsLeft: 3,
-        bgCol: '131, 179, 66'
-    },
-    {
-        name: "Westworld S1 4K Blu-Ray",
-        id: 9,
-        ownerHistory: [],
-        coverImg: P9_C,
-        presentImg: P9_P,
-        stealsLeft: 3,
-        bgCol: '255, 193, 21'
-    },
-    {
-        name: "Lightsaber Chopsticks",
-        id: 10,
-        ownerHistory: [],
-        coverImg: P10_C,
-        presentImg: P10_P,
-        stealsLeft: 3,
-        bgCol: '213, 61, 39'
-    },
-    {
-        name: "Bread Pillow",
-        id: 11,
-        ownerHistory: [],
-        coverImg: P11_C,
-        presentImg: P11_P,
-        stealsLeft: 3,
-        bgCol: '229, 36, 60'
-    },
-    {
-        name: "Bamboo Cheese Board",
-        id: 12,
-        ownerHistory: [],
-        coverImg: P12_C,
-        presentImg: P12_P,
-        stealsLeft: 3,
-        bgCol: '243, 135, 156'
-    },
-    {
-        name: "Reusable Paper Towels",
-        id: 13,
-        ownerHistory: [],
-        coverImg: P13_C,
-        presentImg: P13_P,
-        stealsLeft: 3,
-        bgCol: '242, 121, 143'
-    },
-    {
-        name: "Acupressure Mat",
-        id: 14,
-        ownerHistory: [],
-        coverImg: P14_C,
-        presentImg: P14_P,
-        stealsLeft: 3,
-        bgCol: '177, 148, 197'
-    },
-    {
-        name: "Nachosaurus Chip & Dip Set",
-        id: 15,
-        ownerHistory: [],
-        coverImg: P15_C,
-        presentImg: P15_P,
-        stealsLeft: 3,
-        bgCol: '254, 191, 22'
-    },
-    {
-        name: "Snuggie Wearable Blanket",
-        id: 16,
-        ownerHistory: [],
-        coverImg: P16_C,
-        presentImg: P16_P,
-        stealsLeft: 3,
-        bgCol: '0, 188, 233'
-    },
+    // {
+    //     name: "Belgian Waffle Maker",
+    //     id: 6,
+    //     ownerHistory: [],
+    //     coverImg: P6_C,
+    //     presentImg: P6_P,
+    //     stealsLeft: 3,
+    //     bgCol: '239, 114, 41'
+    // }
+    // {
+    //     name: "Echo Pop Speaker",
+    //     id: 7,
+    //     ownerHistory: [],
+    //     coverImg: P7_C,
+    //     presentImg: P7_P,
+    //     stealsLeft: 3,
+    //     bgCol: '0, 153, 190'
+    // },
+    // {
+    //     name: "Beer Gloves",
+    //     id: 8,
+    //     ownerHistory: [],
+    //     coverImg: P8_C,
+    //     presentImg: P8_P,
+    //     stealsLeft: 3,
+    //     bgCol: '131, 179, 66'
+    // },
+    // {
+    //     name: "Westworld S1 4K Blu-Ray",
+    //     id: 9,
+    //     ownerHistory: [],
+    //     coverImg: P9_C,
+    //     presentImg: P9_P,
+    //     stealsLeft: 3,
+    //     bgCol: '255, 193, 21'
+    // },
+    // {
+    //     name: "Lightsaber Chopsticks",
+    //     id: 10,
+    //     ownerHistory: [],
+    //     coverImg: P10_C,
+    //     presentImg: P10_P,
+    //     stealsLeft: 3,
+    //     bgCol: '213, 61, 39'
+    // },
+    // {
+    //     name: "Bread Pillow",
+    //     id: 11,
+    //     ownerHistory: [],
+    //     coverImg: P11_C,
+    //     presentImg: P11_P,
+    //     stealsLeft: 3,
+    //     bgCol: '229, 36, 60'
+    // },
+    // {
+    //     name: "Bamboo Cheese Board",
+    //     id: 12,
+    //     ownerHistory: [],
+    //     coverImg: P12_C,
+    //     presentImg: P12_P,
+    //     stealsLeft: 3,
+    //     bgCol: '243, 135, 156'
+    // },
+    // {
+    //     name: "Reusable Paper Towels",
+    //     id: 13,
+    //     ownerHistory: [],
+    //     coverImg: P13_C,
+    //     presentImg: P13_P,
+    //     stealsLeft: 3,
+    //     bgCol: '242, 121, 143'
+    // },
+    // {
+    //     name: "Acupressure Mat",
+    //     id: 14,
+    //     ownerHistory: [],
+    //     coverImg: P14_C,
+    //     presentImg: P14_P,
+    //     stealsLeft: 3,
+    //     bgCol: '177, 148, 197'
+    // },
+    // {
+    //     name: "Nachosaurus Chip & Dip Set",
+    //     id: 15,
+    //     ownerHistory: [],
+    //     coverImg: P15_C,
+    //     presentImg: P15_P,
+    //     stealsLeft: 3,
+    //     bgCol: '254, 191, 22'
+    // },
+    // {
+    //     name: "Snuggie Wearable Blanket",
+    //     id: 16,
+    //     ownerHistory: [],
+    //     coverImg: P16_C,
+    //     presentImg: P16_P,
+    //     stealsLeft: 3,
+    //     bgCol: '0, 188, 233'
+    // },
 ]
